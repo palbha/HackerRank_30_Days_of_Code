dd67b9309c26d25fa9b9a854b67f0bd44587a8ca
Sonia <palbhanazwale@gmail.com>
2019-05-14 15:04:48 +0530

    Add files via upload

---
+++ Day 21_Generics.h
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+template <typename Element>
+
+void printArray(vector<Element> arr) {
+  for (int i = 0; i < arr.size(); i++)
+    cout << arr[i] << endl;
+}
+
+
+/**
+*    Name: printArray
+*    Print each element of the generic vector on a new line. Do not return anything.
+*    @param A generic vector
+**/
+
+// Write your code here
+
+int main() {
